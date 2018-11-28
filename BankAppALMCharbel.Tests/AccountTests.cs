27c96044431f033a9f679d17d3df36b2ec9b4c43
Charbel Yakoub <charbel_145@hotmail.com>
2018-11-28 18:00:58 +0100

    Replaced the framework test project to a core test project.
    Created tests on deposit and withdraw

---
+++ BankAppALMCharbel.Tests/AccountTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using BankRepo.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankAppALMCharbel.Tests
+{
+    [TestClass]
+    public class AccountTests
+    {
+        private Account account;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            account = new Account();
+        }
+
+        [TestMethod]
+        public void DepositValidAmount()
+        {
+            account.Deposit(10);
+
+            Assert.AreEqual(10, account.Balance);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DepositInvalidAmount()
+        {
+            account.Deposit(0);
+        }
+
+        [TestMethod]
+        public void WithdrawValidAmount()
+        {
+            account.Balance = 10;
+
+            account.Withdraw(10);
+
+            Assert.AreEqual(0, account.Balance);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WithdrawInvalidAmount()
+        {
+            account.Withdraw(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void WithdrawInsufficient()
+        {
+            account.Withdraw(10);
+        }
+    }
+}
--- BankRepo/Models/Account.cs
+++ BankRepo/Models/Account.cs
@@ -12,11 +12,17 @@ namespace BankRepo.Models
 
         public void Deposit(decimal amount)
         {
+            if (amount < 1) throw new ArgumentException("Amount must be greater than 0!", nameof(amount));
+
             Balance += amount;
         }
 
         public void Withdraw(decimal amount)
         {
+            if (amount < 1) throw new ArgumentException("Amount must be greater than 0!", nameof(amount));
+
+            if (amount > Balance) throw new InvalidOperationException("Amount can not be greater than account balance!");
+
             Balance -= amount;
         }
     }
