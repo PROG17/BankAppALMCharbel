c7aa4505d6e52649103a4d9d944dc4bcdc0e517b
kalle (jag) <kalle.jillheden@me.com>
2018-11-29 19:01:46 +0100

    Resolved unit tests

--- BankRepo/Models/Account.cs
+++ BankRepo/Models/Account.cs
@@ -28,7 +28,16 @@ namespace BankRepo.Models
 
         public void Transfer(decimal amount, Account recipient)
         {
-            throw new NotImplementedException();
+            if (amount <= 0) throw new ArgumentOutOfRangeException(nameof(amount), "Amount must be greater than 0!");
+
+            if (recipient is null) throw new ArgumentNullException(nameof(recipient), "Transfer recipient cannot be null.");
+            
+            if (ReferenceEquals(recipient, this)) throw new ArgumentException("Transfer recipient cannot be same account.", nameof(recipient));
+
+            if (amount > Balance) throw new InvalidOperationException("Amount cannot be greater than account balance!");
+
+            Balance -= amount;
+            recipient.Balance += amount;
         }
     }
 }
